942cf98db501b9cdb59c3a8950ba535a8f14099f
서주안 <seojuan@seojuan-ui-MacBookAir.local>
2021-09-16 14:15:59 +0900

    v1

---
+++ src/AccountTest/Account.java
@@ -0,0 +1,19 @@
+package AccountTest;
+
+public class Account {
+	private double balance = 0;
+	
+	public Account(double initialBalance) {
+		if(initialBalance > 0.0) {
+			balance = initialBalance;
+		}
+	}
+	
+	public void credit(double amount) {
+		balance = balance + amount;
+	}
+	
+	public double getBalance() {
+		return balance;
+	}
+}
